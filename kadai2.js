1abd683538bf208678bf775cc89f14f52d99f8df
CIT-YamatoTakeda <71809768+CIT-YamatoTakeda@users.noreply.github.com>
2021-05-23 14:03:21 +0900

    とりあえず完成

--- kadai2.js
+++ kadai2.js
@@ -1,7 +1,6 @@
 var init = function() {
 
-    var width = 1280,
-        height = 720;
+    var width = 1280, height = 720;
   
     // レンダラーを作成
     var renderer = new THREE.WebGLRenderer();
@@ -14,31 +13,49 @@ var init = function() {
     // カメラを作成
     var camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
   
-    // 箱を作成
-    var geometry = new THREE.OctahedronGeometr(200);
-    var material = new THREE.MeshLambertMaterial({ color: 0x0000ff });
-    var octahedron = new THREE.Mesh(geometry, material);
-    octahedron.position.z = -5;
-    scene.add(octahedron);
+    // 八角形1
+    var geometry = new THREE.OctahedronGeometry(1);
+    var material = new THREE.MeshBasicMaterial({color: 0xa0d8ef});
+    var octahedron1 = new THREE.Mesh(geometry, material);
+    octahedron1.position.set(-2, 0, -5);
+    scene.add(octahedron1);
+    // 八角形2
+    material = new THREE.MeshLambertMaterial({color: 0xd70035});
+    var octahedron2 = new THREE.Mesh(geometry, material);
+    octahedron2.position.set(0, 0, -5);
+    scene.add(octahedron2);
+    // 八角形3
+    material = new THREE.MeshPhongMaterial({color: 0x00a968});
+    var octahedron3 = new THREE.Mesh(geometry, material);
+    octahedron3.position.set(2, 0, -5);
+    scene.add(octahedron3);
   
     // 平行光源1
     var directionalLight1 = new THREE.DirectionalLight(0xffffff);
     directionalLight1.position.set(1, 1, 1);
     // シーンに追加
-    scene.add(directionalLight1);
+    //scene.add(directionalLight1);
     // 平行光源2
-        var directionalLight2 = new THREE.DirectionalLight(0xffffff);
+    var directionalLight2 = new THREE.DirectionalLight(0xffffff);
     directionalLight2.position.set(-1, 1, 1);
     // シーンに追加
-    scene.add(directionalLight2);
+    //scene.add(directionalLight2);
+    // 点光源
+    var PointLight = new THREE.PointLight(0xffffff, 5, 50, 1);
+    PointLight.position.set(20, 5, 20);
+    scene.add(PointLight);
   
     // 初回実行
     var update = function() {
       requestAnimationFrame(update);
   
       // 箱を回転させる
-      octahedron.rotation.x += 0.01;
-      octahedron.rotation.y += 0.01;
+      octahedron1.rotation.x += 0.01;
+      octahedron1.rotation.y += 0.01;
+      octahedron2.rotation.x -= 0.01;
+      octahedron2.rotation.y -= 0.01;
+      octahedron3.rotation.x += 0.01;
+      octahedron3.rotation.y += 0.01;
   
       renderer.render(scene, camera);
     };
