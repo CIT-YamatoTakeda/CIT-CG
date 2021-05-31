b8c350fe5db3b8be468814b256444b3c8771f392
CIT-YamatoTakeda <71809768+CIT-YamatoTakeda@users.noreply.github.com>
2021-05-31 17:47:14 +0900

    リンク修正

--- 7-1.js
+++ 7-1.js
@@ -16,7 +16,7 @@ var init = function() {
 
 // テクスチャー読み込み
 var textureLoader = new THREE.TextureLoader();
-  var texture = textureLoader.load("img/bricks.jpg");
+  var texture = textureLoader.load("img/Bricks.jpg");
   var mat = new THREE.MeshPhongMaterial();
   mat.map = texture;
 
